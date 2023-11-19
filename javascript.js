bd65c09aaca99e6b8ecdf614ceeab0a413ae3f77
Linvicky0 <lvicky@umich.edu>
2023-11-18 22:24:26 -0500

    complete view: included desktop

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function expandIcon(element) {
+alert(element.nextElementSibling.innerHTML)
+}
+images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('click', function(event) {
+        if (event.target.id == 'largeImage') {
+            event.target.removeAttribute('id', 'largeImage');
+        }
+        else {
+       image.setAttribute('id', 'largeImage');
+       console.log(image.style.width)
+        }
+});
+});
