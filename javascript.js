df308d6f2363e6e78843f8d708f74c819b99dbf7
Linvicky0 <lvicky@umich.edu>
2023-11-19 19:10:43 -0500

    fixed errors

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
+
+// info = document.getElementsByClassName('fas fa-info-circle');
+// info.forEach(icon => {
+//     icon.addEventListener('focus', function(event) {
+//         icon.nextElementSibling.style.display = 'block'
+//     })
+// })
+/*
 function expandIcon(element) {
-alert(element.nextElementSibling.innerHTML);
-}
+    alert(element.nextElementSibling.innerHTML);
+    }
+    */
 images = document.querySelectorAll('img')
 images.forEach(image => {
     image.addEventListener('click', function(event) {
